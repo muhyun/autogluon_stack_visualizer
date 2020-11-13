fe9d29f406fd8c26b6dbd9d186f8127cf709af29
muhyun <muhyun.kim@gmail.com>
2020-11-13 23:14:22 +0900

    stack visualizer

--- autogluon_stack_visualizer.py
+++ autogluon_stack_visualizer.py
@@ -1,15 +1,14 @@
-# How to visualize the stack ensemble model of AutoGluon-Tabular
+from autogluon import TabularPrediction as task
+import networkx as nx
+from PIL import Image
 
-[AutoGluon Tabular](https://auto.gluon.ai/stable/index.html) trains a state-of-the-art tabular model using stacking ensemble model. Here I introduce a simple Graphviz based function to visualize the stack ensemble model.
-
-```python
 def generate_model_visual(predictor, model_image_fname='model.png'):
     G = predictor._trainer.model_graph
     remove = [node for node,degree in dict(G.degree()).items() if degree < 1]
     G.remove_nodes_from(remove)
     root_node = [n for n,d in G.out_degree() if d==0]
     best_model_node = predictor.get_model_best()
-
+    
     A = nx.nx_agraph.to_agraph(G)
     A.graph_attr['label'] = 'Ensemble stack (Blue box is the best model)'
     A.graph_attr['labelloc']='t'
@@ -19,7 +18,7 @@ def generate_model_visual(predictor, model_image_fname='model.png'):
     A.node_attr.update(shape='rectangle')
     for node in A.iternodes():
         node.attr['label'] = f"{node.name}\nVal score: {float(node.attr['val_score']):.4f}"
-
+        
         if node.name == best_model_node:
             node.attr['style'] = 'filled'
             node.attr['fillcolor'] = '#ff9900'
@@ -27,15 +26,16 @@ def generate_model_visual(predictor, model_image_fname='model.png'):
         elif nx.has_path(G, node.name, best_model_node):
             node.attr['style'] = 'filled'
             node.attr['fillcolor'] = '#ffcc00'
-
+            
     A.draw(model_image_fname, format='png', prog='dot')
-```
 
-This is a stacked ensemble model trained by executing `fit()`.
+train_data = task.Dataset(file_path='./train.csv')
 
-![](./model_plain.png)
+predictor = task.fit(train_data=train_data, label='y', time_limits=60, 
+                     stack_ensemble_levels=2, num_bagging_folds=2)
 
+generate_model_visual(predictor, './model_plain.png')
 
-This is a new stacked ensemble model created by `fit_weighted_ensemble()` on the previous trained model. As seen here, this helps understading how AutoGluon stacks models to get the best model.
+predictor.fit_weighted_ensemble()
+generate_model_visual(predictor, './model_weighted_ensemble.png')
 
-![](./model_weighted_ensemble.png)
